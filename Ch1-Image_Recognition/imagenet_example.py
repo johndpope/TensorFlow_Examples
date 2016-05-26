6a8717aae59543240a3ee6ab17e1845e1d2b0124
SOLARIS <jinhoyang@snu.ac.kr>
2016-05-27 01:27:45 +0900

    Update imagenet_example.py

--- Ch1-Image_Recognition/imagenet_example.py
+++ Ch1-Image_Recognition/imagenet_example.py
@@ -171,6 +171,7 @@ def maybe_download_and_extract():
     statinfo = os.stat(filepath)
     print('Succesfully downloaded', filename, statinfo.st_size, 'bytes.')
   tarfile.open(filepath, 'r:gz').extractall(dest_directory)
+
   
 def main(argv=None):
   # Inception-v3 모델을 다운로드하고 압축을 푼다.
